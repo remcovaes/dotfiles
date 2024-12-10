a0f28b162b24df599c46c5764351f553551df2ed
Remco Vaes <remco.vaes@brightcubes.nl>
2024-12-10 10:00:38 +0100

    disable it

--- nvim/init.lua
+++ nvim/init.lua
@@ -932,7 +932,7 @@ require("lazy").setup({
 				-- Disable "format_on_save lsp_fallback" for languages that don't
 				-- have a well standardized coding style. You can add additional
 				-- languages here or re-enable it for the disabled ones.
-				local disable_filetypes = { c = true, cpp = true, ts_ls = true, eslint = true }
+				local disable_filetypes = { c = true, cpp = true, ts = true, tsx = true, js = true, jsx = true }
 				local lsp_format_opt
 				if disable_filetypes[vim.bo[bufnr].filetype] then
 					lsp_format_opt = "never"
