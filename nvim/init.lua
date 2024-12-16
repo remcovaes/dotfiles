eb4afd20ee850fe52d5c6518d34c9b9fe3894bbc
Remco Vaes <remco.vaes@brightcubes.nl>
2024-12-16 08:41:58 +0100

    Add python refactorings

--- nvim/init.lua
+++ nvim/init.lua
@@ -388,7 +388,6 @@ require("lazy").setup({
 			vim.keymap.set("n", "<leader>td", function()
 				require("dap-python").test_method()
 			end, { desc = "Debug test" })
-
 			dap.listeners.before.attach.dapui_config = function()
 				ui.open()
 			end
@@ -401,6 +400,29 @@ require("lazy").setup({
 			dap.listeners.before.event_exited.dapui_config = function()
 				ui.close()
 			end
+		end,
+	},
+	{
+		"ThePrimeagen/refactoring.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		lazy = false,
+		config = function()
+			require("refactoring").setup()
+
+			vim.keymap.set("x", "<leader>re", ":Refactor extract ")
+			vim.keymap.set("x", "<leader>rf", ":Refactor extract_to_file ")
+
+			vim.keymap.set("x", "<leader>rv", ":Refactor extract_var ")
+
+			vim.keymap.set({ "n", "x" }, "<leader>ri", ":Refactor inline_var")
+
+			vim.keymap.set("n", "<leader>rI", ":Refactor inline_func")
+
+			vim.keymap.set("n", "<leader>rb", ":Refactor extract_block")
+			vim.keymap.set("n", "<leader>rbf", ":Refactor extract_block_to_file")
 		end,
 	},
 
